6d3db0a1a34b3e5a127799cd58c53f33e465fb96
ivan7182 <muhammadivan7182@gmail.com>
2025-03-16 08:34:41 +0700

    UnitTest

--- Helper/hello_world_test.go
+++ Helper/hello_world_test.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -47,14 +48,83 @@ func TestHelloWorldEko(t *testing.T) {
 	fmt.Println("hello Eko done")
 }
 
+// Assert
 func TestHelloWorldAssert(t *testing.T) {
 	value := HelloWorld("ivan")
 	assert.Equal(t, "Hello ivan", value, "tidak sama dengan 'hello ivan'")
 	fmt.Println("done dengan test assert")
 }
 
+// Require
 func TestHelloWorldRequire(t *testing.T) {
 	value := HelloWorld("ivan")
 	require.Equal(t, "Hello ivan", value, "tidak sama dengan hello ivan")
 	fmt.Println("Test Hello World with require done")
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "ivan" {
+		t.Skip("can not on winndows")
+	}
+
+	value := HelloWorld("skip")
+	require.Equal(t, "hello ivan", value, "tidak sama valuenya")
+}
+
+//After Before Test
+
+func TestMain(m *testing.M) {
+	//Before
+	fmt.Println("Before unit test")
+	m.Run()
+
+	//after
+	fmt.Println("After unit test")
+}
+
+//Test Subtest
+
+func TestSubTest(t *testing.T) {
+	t.Run("ivan", func(t *testing.T) {
+		value := HelloWorld("ivan")
+		assert.Equal(t, "Hello joko", value, "tidak sama dengan value")
+	})
+
+	t.Run("satria", func(t *testing.T) {
+		value := HelloWorld("satria")
+		require.Equal(t, "Hello satria", value, "tidak sama dengan value")
+	})
+}
+
+//Table Test
+
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "ivan",
+			request:  "ivan",
+			expected: "Hello ivan",
+		},
+		{
+			name:     "satria",
+			request:  "satria",
+			expected: "Hello satria",
+		},
+		{
+			name:     "joko",
+			request:  "joko",
+			expected: "Hello joko",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			value := HelloWorld(test.request)
+			require.Equal(t, test.expected, value)
+		})
+	}
 }
