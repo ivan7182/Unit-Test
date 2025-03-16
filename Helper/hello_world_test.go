36a623d2694dfa834e8f9d3dab665d69667e6aac
ivan7182 <muhammadivan7182@gmail.com>
2025-03-16 07:05:24 +0700

    UnitTest

--- Helper/hello_world_test.go
+++ Helper/hello_world_test.go
@@ -1,11 +1,34 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+// /Fail
+func TestHelloWorldIvan(t *testing.T) {
 	value := HelloWorld("ivan")
 
-	if value != "Hello ivan" {
-		panic("Value tidak 'Hello ivan")
+	if value != "hello ivan" {
+		t.Fail()
+	}
+	fmt.Println("hello ivan done")
+}
+
+// FailNow
+func TestHelloWorldSatria(t *testing.T) {
+	value := HelloWorld("satria")
+
+	if value != "hello satria" {
+		t.FailNow()
+	}
+	fmt.Println("hello satria done")
+}
+
+func TestHelloWorldJoko(t *testing.T) {
+	value := HelloWorld("joko")
+
+	if value != "Hello Joko" {
+		t.Error("value tidak 'Hello Joko'")
 	}
 }
