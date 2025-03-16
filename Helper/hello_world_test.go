7474cc965204736d130df224da480b476c14dd9b
ivan7182 <muhammadivan7182@gmail.com>
2025-03-16 07:37:14 +0700

    UnitTest

--- Helper/hello_world_test.go
+++ Helper/hello_world_test.go
@@ -3,6 +3,9 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // /Fail
@@ -25,10 +28,33 @@ func TestHelloWorldSatria(t *testing.T) {
 	fmt.Println("hello satria done")
 }
 
+// Error
 func TestHelloWorldJoko(t *testing.T) {
 	value := HelloWorld("joko")
 
 	if value != "Hello Joko" {
 		t.Error("value tidak 'Hello Joko'")
 	}
+	fmt.Println("hello joko done")
+}
+
+func TestHelloWorldEko(t *testing.T) {
+	value := HelloWorld("Eko")
+
+	if value != "Hello Eko" {
+		t.Fatal("value tidak 'Hello Eko'")
+	}
+	fmt.Println("hello Eko done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	value := HelloWorld("ivan")
+	assert.Equal(t, "Hello ivan", value, "tidak sama dengan 'hello ivan'")
+	fmt.Println("done dengan test assert")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	value := HelloWorld("ivan")
+	require.Equal(t, "Hello ivan", value, "tidak sama dengan hello ivan")
+	fmt.Println("Test Hello World with require done")
 }
